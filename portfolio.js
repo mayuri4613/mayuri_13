796e96aca6c148df8348f7c54a51bdc99ede0182
mayuri4613 <161993462+mayuri4613@users.noreply.github.com>
2024-08-19 14:39:11 +0530

    Add files via upload
    This is a Portfolio structure using HTML and CSS along with Javascript. This Portfolio has nested HTML pages and each page has its own styling.

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+<script>
+    document.querySelectorAll('a').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+</script>
